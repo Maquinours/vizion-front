a29b1655d1879fe4e3b22349de15a67f37503c06
Maquinours <juju9559@hotmail.fr>
2024-02-29 11:28:21 +0100

    warning fixes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
-import { createRootRoute } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import '../assets/styles/_vizion.style.scss';
-import { Outlet } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   component: () => <Outlet />,
