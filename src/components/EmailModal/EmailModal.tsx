a089eb48ebfd0a11c91638e9aee45f409a4ad8a2
Maquinours <juju9559@hotmail.fr>
2024-03-27 12:04:25 +0100

    EmailModal fix
    EmailModal now allow outlet to handle replyModal.

--- src/components/EmailModal/EmailModal.tsx
+++ src/components/EmailModal/EmailModal.tsx
@@ -4,7 +4,7 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { emailQueryKeys } from '../../utils/constants/queryKeys/email';
 import { getEmailById } from '../../utils/api/email';
 import { PUBLIC_BASE_URL } from '../../utils/constants/api';
-import { Link, LinkProps } from '@tanstack/react-router';
+import { Link, LinkProps, Outlet } from '@tanstack/react-router';
 import styles from './EmailModal.module.scss';
 
 type EmailModalComponentProps = Readonly<{
@@ -19,55 +19,56 @@ export default function EmailModalComponent({ onClose, emailId, replyLink }: Ema
   });
 
   return (
-    <ReactModal isOpen={true} onRequestClose={onClose} className={styles.mail_modal} overlayClassName="Overlay">
-      <div className={styles.modal_container}>
-        <div className={styles.modal_title}>
-          <p>Contenu du mail</p>
-        </div>
-        <div className={styles.modal_content}>
-          <div className={styles.attached_files}>
-            <div>
-              <p>A : {email?.receiver?.split(';').join(' ')}</p>
-              <p>
-                De : <a href={`mailto:${email?.sender}`}>{email?.sender}</a>
-              </p>
-              <h4>Pièces jointes : </h4>
-              {email.pjList?.length > 0 ? (
-                <ul>
-                  {email.pjList?.map((item) => (
-                    <>
+    <>
+      <ReactModal isOpen={true} onRequestClose={onClose} className={styles.mail_modal} overlayClassName="Overlay">
+        <div className={styles.modal_container}>
+          <div className={styles.modal_title}>
+            <p>Contenu du mail</p>
+          </div>
+          <div className={styles.modal_content}>
+            <div className={styles.attached_files}>
+              <div>
+                <p>A : {email?.receiver?.split(';').join(' ')}</p>
+                <p>
+                  De : <a href={`mailto:${email?.sender}`}>{email?.sender}</a>
+                </p>
+                <h4>Pièces jointes : </h4>
+                {email.pjList?.length > 0 ? (
+                  <ul>
+                    {email.pjList?.map((item) => (
                       <li style={{ marginBottom: '5px', cursor: 'pointer' }}>
                         <a href={`${PUBLIC_BASE_URL}mail/v1/download-file/${item.name}?ref=${email.id}`} target="_blank" rel="noopener noreferrer">
                           {item.name}
                         </a>
                       </li>
-                    </>
-                  ))}
-                </ul>
-              ) : (
-                <div style={{ marginBottom: '1rem' }}>Aucune pièce jointe</div>
+                    ))}
+                  </ul>
+                ) : (
+                  <div style={{ marginBottom: '1rem' }}>Aucune pièce jointe</div>
+                )}
+              </div>
+              <div className={styles.receiveDate}>Envoyé le : {formatDateWithHour(email?.sendDate)}</div>
+            </div>
+
+            <div className={styles.mailbox}>
+              <div dangerouslySetInnerHTML={{ __html: email.content }} />
+            </div>
+          </div>
+          <div className={styles.modal_footer}>
+            <div className={styles.buttons_container}>
+              <button className="btn btn-secondary" onClick={() => onClose()}>
+                Fermer
+              </button>
+              {replyLink && (
+                <Link {...replyLink} className="btn btn-primary">
+                  Répondre
+                </Link>
               )}
             </div>
-            <div className={styles.receiveDate}>Envoyé le : {formatDateWithHour(email?.sendDate)}</div>
-          </div>
-
-          <div className={styles.mailbox}>
-            <div dangerouslySetInnerHTML={{ __html: email.content }} />
           </div>
         </div>
-        <div className={styles.modal_footer}>
-          <div className={styles.buttons_container}>
-            <button className="btn btn-secondary" onClick={() => onClose()}>
-              Fermer
-            </button>
-            {replyLink && (
-              <Link {...replyLink} className="btn btn-primary">
-                Répondre
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
-    </ReactModal>
+      </ReactModal>
+      <Outlet />
+    </>
   );
 }
