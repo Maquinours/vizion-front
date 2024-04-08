9e10b4b9a9bf061b9bcbe68482e22d10bf3f255d
Maquinours <juju9559@hotmail.fr>
2024-04-08 14:08:01 +0200

    keycloak event initial key change

--- src/utils/constants/queryKeys/keycloakEvent.ts
+++ src/utils/constants/queryKeys/keycloakEvent.ts
@@ -1,7 +1,7 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 import { getLatestKeycloakEvents } from '../../../views/App/views/Dashboard/components/LatestConnections/utils/api/keycloakEvents';
 
-export const keycloakEvents = createQueryKeys('keycloak-events', {
+export const keycloakEvents = createQueryKeys('keycloak-event', {
   page: ({ page, size }: { page: number; size: number }) => ({
     queryKey: [page, size],
     queryFn: () => getLatestKeycloakEvents(page, size),
