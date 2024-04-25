688d18dbde2c74f19e135ff38217f33753d6103d
Maquinours <juju9559@hotmail.fr>
2024-04-25 10:24:35 +0200

    temporary remove sentry plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin';
+// import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
@@ -8,14 +8,14 @@ export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite(),
-    sentryVitePlugin({
-      org: 'vizeo',
-      project: 'javascript-react',
-    }),
+    // sentryVitePlugin({
+    //   org: 'vizeo',
+    //   project: 'javascript-react',
+    // }),
   ],
 
   build: {
-    sourcemap: true,
+    // sourcemap: true,
     rollupOptions: {
       onwarn(warning, defaultHandler) {
         if (warning.code === 'SOURCEMAP_ERROR') {
