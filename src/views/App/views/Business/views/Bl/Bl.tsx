71a741ef792b70396b8b100fd22265476a90f9ab
Maquinours <juju9559@hotmail.fr>
2024-04-26 15:37:57 +0200

    bl pagination optimization

--- src/views/App/views/Business/views/Bl/Bl.tsx
+++ src/views/App/views/Business/views/Bl/Bl.tsx
@@ -1,16 +1,14 @@
-import { Outlet, getRouteApi, useNavigate } from '@tanstack/react-router';
-import AppViewBusinessViewBlViewBodyComponent from './components/Body/Body';
-import AppViewBusinessViewBlViewHeaderComponent from './components/Header/Header';
-import PaginationComponent from '../../../../../../components/Pagination/Pagination';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { Outlet, getRouteApi } from '@tanstack/react-router';
+import PaginationComponent from '../../../../../../components/Pagination/Pagination';
 import { queries } from '../../../../../../utils/constants/queryKeys';
 import styles from './Bl.module.scss';
+import AppViewBusinessViewBlViewBodyComponent from './components/Body/Body';
+import AppViewBusinessViewBlViewHeaderComponent from './components/Header/Header';
 
 const routeApi = getRouteApi('/app/businesses-rma/business/$businessId/bl');
 
 export default function AppViewBusinessViewBlView() {
-  const navigate = useNavigate({ from: routeApi.id });
-
   const { businessId } = routeApi.useParams();
   const { page } = routeApi.useSearch();
 
@@ -22,7 +20,7 @@ export default function AppViewBusinessViewBlView() {
         <AppViewBusinessViewBlViewHeaderComponent />
         <AppViewBusinessViewBlViewBodyComponent />
         <div>
-          <PaginationComponent page={page} totalPages={bls.length} onPageChange={(page) => navigate({ search: (old) => ({ ...old, page }) })} />
+          <PaginationComponent page={page} totalPages={bls.length} pageLink={(page) => ({ search: (old) => ({ ...old, page }) })} />
         </div>
       </div>
       <Outlet />
