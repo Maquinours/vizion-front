53d39b6b6a9d864d6fd6877e3a623e65be1b298a
Maquinours <79869121+Maquinours@users.noreply.github.com>
2024-04-27 17:10:31 +0200

    update business responsible perm fix

--- src/routes/app/businesses-rma/business.$businessId/dashboard/update-responsible/route.ts
+++ src/routes/app/businesses-rma/business.$businessId/dashboard/update-responsible/route.ts
@@ -4,6 +4,6 @@ import { queries } from '../../../../../../utils/constants/queryKeys';
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/dashboard/update-responsible')({
   beforeLoad: async ({ context: { queryClient } }) => {
     const user = await queryClient.ensureQueryData(queries.user.authentified());
-    if (user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO')) throw redirect({ from: Route.id, to: '..', search: (old) => old, replace: true });
+    if (!user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO')) throw redirect({ from: Route.id, to: '..', search: (old) => old, replace: true });
   },
 });
