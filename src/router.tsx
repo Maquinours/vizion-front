d16ba18f5ae4230734f3b048db34c73f27f4f451
Maquinours <79869121+Maquinours@users.noreply.github.com>
2024-04-28 21:45:33 +0200

    remove useless code

--- src/router.tsx
+++ src/router.tsx
@@ -11,15 +11,6 @@ declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
-}
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-declare module '@tanstack/react-router' {
   interface StaticDataRouteOption {
     title?: string;
   }
