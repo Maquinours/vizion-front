f5dae97e038200e4c615538c39fbb196748a7803
Maquinours <juju9559@hotmail.fr>
2024-05-10 16:54:35 +0200

    emails page loader

--- src/routes/app/tools/emails/route.ts
+++ src/routes/app/tools/emails/route.ts
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
+import { queries } from '../../../../utils/constants/queryKeys';
 
 const searchSchema = z.object({
   search: z.string().optional().catch(undefined),
@@ -9,6 +10,10 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/app/tools/emails')({
   validateSearch: searchSchema,
+  loaderDeps: ({ search: { search, spam, page } }) => ({ search, spam, page, size: 15 }),
+  loader: ({ context: { queryClient }, deps: { search, spam, page, size } }) => {
+    queryClient.prefetchQuery(queries.emails.page._ctx.bySpamStateAndSearch(spam ?? false, search, { page, size }));
+  },
   staticData: {
     title: 'Emails',
   },
