726310379812369ac148c069e071c843a5a1343e
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:21:06 +0200

    ddns page loader initial data

--- src/routes/app/tools/ddns/delete.$ddnsId/route.ts
+++ src/routes/app/tools/ddns/delete.$ddnsId/route.ts
@@ -1,8 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { ddns } from '../../../../../utils/constants/queryKeys/ddns';
+import { QueryKey } from '@tanstack/react-query';
+import Page from '../../../../../utils/types/Page';
+import DnsEntryResponseDto from '../../../../../utils/types/DnsEntryResponseDto';
 
 export const Route = createFileRoute('/app/tools/ddns/delete/$ddnsId')({
-  loader: ({ context: { queryClient }, params: { ddnsId } }) => {
-    queryClient.ensureQueryData(ddns.detail(ddnsId));
+  loader: async ({ context: { queryClient }, params: { ddnsId } }) => {
+    let initialDataKey: QueryKey | undefined = undefined;
+
+    await queryClient.ensureQueryData({
+      ...ddns.detail(ddnsId),
+      initialData: () => {
+        for (const [key, value] of queryClient.getQueriesData<Page<DnsEntryResponseDto>>({ queryKey: ddns.page._def })) {
+          const item = value?.content.find((item) => item.id === ddnsId);
+          if (item) {
+            initialDataKey = key;
+            return item;
+          }
+        }
+      },
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
+    });
   },
 });
