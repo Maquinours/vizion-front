c8cfebf27fd7491371409f3984a90e2d522f55d6
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:22:09 +0200

    mails paper pages loader initial data

--- src/routes/app/tools/mails/update.$mailId/route.ts
+++ src/routes/app/tools/mails/update.$mailId/route.ts
@@ -1,8 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { mailQueryKeys } from '../../../../../utils/constants/queryKeys/mails';
+import { QueryKey } from '@tanstack/react-query';
+import Page from '../../../../../utils/types/Page';
+import MailPaperResponseDto from '../../../../../utils/types/MailPaperResponseDto';
 
 export const Route = createFileRoute('/app/tools/mails/update/$mailId')({
   loader: async ({ context: { queryClient }, params: { mailId } }) => {
-    return { mail: await queryClient.ensureQueryData(mailQueryKeys.detail._ctx.byId(mailId)) };
+    let initialDataKey: QueryKey | undefined = undefined;
+
+    const mail = await queryClient.ensureQueryData({
+      ...mailQueryKeys.detail._ctx.byId(mailId),
+      initialData: () => {
+        for (const [key, value] of queryClient.getQueriesData<Page<MailPaperResponseDto>>({ queryKey: mailQueryKeys.page.queryKey })) {
+          const item = value?.content.find((item) => item.id === mailId);
+          if (item) {
+            initialDataKey = key;
+            return item;
+          }
+        }
+      },
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
+    });
+
+    return { mail };
   },
 });
