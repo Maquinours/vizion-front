de52a6fce5e472b227abd0c2a01f7730bfa3dfed
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:23:44 +0200

    representatives-turnover loader optimizations

--- src/routes/app/tools/representatives-turnover/route.ts
+++ src/routes/app/tools/representatives-turnover/route.ts
@@ -4,6 +4,7 @@ import YEARS from '../../../../utils/constants/years';
 import { getAvailableMonthsForYear } from '../../../../utils/functions/moment';
 import { enterprises } from '../../../../utils/constants/queryKeys/enterprise';
 import CategoryClient from '../../../../utils/enums/CategoryClient';
+import { queries } from '../../../../utils/constants/queryKeys';
 
 const searchSchema = z.object({
   representativeId: z.string().optional().catch(undefined),
@@ -23,14 +24,25 @@ export const Route = createFileRoute('/app/tools/representatives-turnover')({
     if (!getAvailableMonthsForYear(values.year).at(values.month - 1)) values.month = getAvailableMonthsForYear(values.year).length;
     return values;
   },
-  loaderDeps: ({ search }) => ({ search }),
-  loader: async ({ context: { queryClient }, deps: { search } }) => {
+  loaderDeps: ({ search: { representativeId, year, month } }) => ({ representativeId, year, month }),
+  loader: async ({ context: { queryClient }, deps: { representativeId, year, month } }) => {
     const representatives = await queryClient.ensureQueryData(enterprises.list._ctx.byCategory(CategoryClient.REPRESENTANT));
-    if (search.representativeId && !representatives.some((representative) => representative.id === search.representativeId))
-      throw redirect({
-        from: Route.id,
-        search: { ...search, representativeId: undefined },
-      });
+    if (representativeId) {
+      const representative = representatives.find((rep) => rep.id === representativeId);
+      if (!representative)
+        throw redirect({
+          from: Route.id,
+          search: (prev) => ({ ...prev, representativeId: undefined }),
+        });
+
+      queryClient.prefetchQuery(
+        queries['sales-vva'].list._ctx.byDepartmentCodesYearAndMonth({
+          departmentCodes: representative.departments?.map((dep) => dep.code) ?? [],
+          year,
+          month,
+        }),
+      );
+    }
   },
   staticData: {
     title: 'CA des représentants',
