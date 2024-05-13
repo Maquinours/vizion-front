eabbd5b543f25d99b0a56b0cd833b58107c7dea4
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:21:23 +0200

    email page loader

---
+++ src/routes/app/tools/emails/$emailId/route.ts
@@ -0,0 +1,26 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { queries } from '../../../../../utils/constants/queryKeys';
+import { QueryKey } from '@tanstack/react-query';
+import Page from '../../../../../utils/types/Page';
+import MailResponseDto from '../../../../../utils/types/MailResponseDto';
+
+export const Route = createFileRoute('/app/tools/emails/$emailId')({
+  loader: async ({ context: { queryClient }, params: { emailId } }) => {
+    let initialDataKey: QueryKey | undefined = undefined;
+    await queryClient.ensureQueryData({
+      ...queries.emails.detail(emailId),
+      initialData: () => {
+        for (const [key, value] of queryClient.getQueriesData<Page<MailResponseDto>>({
+          queryKey: queries.emails.page.queryKey,
+        })) {
+          const item = value?.content.find((item) => item.id === emailId);
+          if (item) {
+            initialDataKey = key;
+            return item;
+          }
+        }
+      },
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
+    });
+  },
+});
