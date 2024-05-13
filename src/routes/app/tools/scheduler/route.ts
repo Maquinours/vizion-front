e3fdff4f806b661c210f106dc84c1e2be066799a
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:23:54 +0200

    scheduler page loader

--- src/routes/app/tools/scheduler/route.ts
+++ src/routes/app/tools/scheduler/route.ts
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Views } from 'react-big-calendar';
 import { z } from 'zod';
+import { queries } from '../../../../utils/constants/queryKeys';
 
 const searchSchema = z.object({
   view: z.enum([Views.DAY, Views.WORK_WEEK, Views.MONTH, Views.WORK_WEEK, Views.AGENDA]).catch(Views.DAY),
@@ -9,6 +10,9 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/app/tools/scheduler')({
   validateSearch: searchSchema,
+  loader: ({ context: { queryClient } }) => {
+    queryClient.prefetchQuery(queries['rdv-user-infos'].list);
+  },
   staticData: {
     title: 'Agenda',
   },
