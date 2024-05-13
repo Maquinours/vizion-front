6c6450a40e1eac4260c7a0f44a93e8523670fea2
Maquinours <juju9559@hotmail.fr>
2024-05-13 15:23:20 +0200

    predefined texts pages loader initial data fix

--- src/routes/app/tools/predefined-texts/update.$predefinedTextId/route.ts
+++ src/routes/app/tools/predefined-texts/update.$predefinedTextId/route.ts
@@ -2,22 +2,24 @@ import { createFileRoute } from '@tanstack/react-router';
 import { queries } from '../../../../../utils/constants/queryKeys';
 import PredefinedTextResponseDto from '../../../../../utils/types/PredefinedTextResponseDto';
 import { QueryKey } from '@tanstack/react-query';
+import Page from '../../../../../utils/types/Page';
 
 export const Route = createFileRoute('/app/tools/predefined-texts/update/$predefinedTextId')({
   loader: async ({ context: { queryClient }, params: { predefinedTextId } }) => {
-    let initialDataKey: QueryKey;
+    let initialDataKey: QueryKey | undefined = undefined;
 
     await queryClient.ensureQueryData({
       ...queries['predefined-text'].detail(predefinedTextId),
       initialData: () => {
-        for (const [key, value] of queryClient.getQueriesData<PredefinedTextResponseDto>({ queryKey: queries['predefined-text'].page._def })) {
-          if (value?.id === predefinedTextId) {
+        for (const [key, value] of queryClient.getQueriesData<Page<PredefinedTextResponseDto>>({ queryKey: queries['predefined-text'].page._def })) {
+          const item = value?.content.find((item) => item.id === predefinedTextId);
+          if (item) {
             initialDataKey = key;
             return value;
           }
         }
       },
-      initialDataUpdatedAt: () => queryClient.getQueryState(initialDataKey)?.dataUpdatedAt,
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
     });
   },
 });
