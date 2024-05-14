c8f2007dd6d2a16809278a198b0afd9568d94ef5
Maquinours <juju9559@hotmail.fr>
2024-05-14 08:54:59 +0200

    router devtools

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,11 +1,26 @@
 import { Outlet, ScrollRestoration } from '@tanstack/react-router';
+import React, { Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    );
 
 export default function RootView() {
   return (
     <>
       <ScrollRestoration />
       <ToastContainer position={'bottom-right'} hideProgressBar={true} theme={'colored'} autoClose={3000} />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
       <Outlet />
     </>
   );
