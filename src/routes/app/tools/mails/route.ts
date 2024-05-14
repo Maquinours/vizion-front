66479b07d8b58e777d95b2fa416596211c2f7dc3
Maquinours <juju9559@hotmail.fr>
2024-05-14 12:04:42 +0200

    mails page loader

--- src/routes/app/tools/mails/route.ts
+++ src/routes/app/tools/mails/route.ts
@@ -1,5 +1,6 @@
 import { SearchSchemaInput, createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
+import { mailQueryKeys } from '../../../../utils/constants/queryKeys/mails';
 
 const searchSchema = z.object({
   search: z.string().optional().catch(undefined),
@@ -8,4 +9,8 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/app/tools/mails')({
   validateSearch: (data: { search?: string; page?: number } & SearchSchemaInput) => searchSchema.parse(data),
+  loaderDeps: ({ search: { search, page } }) => ({ search, page, size: 20 }),
+  loader: ({ context: { queryClient }, deps: { search, page, size } }) => {
+    queryClient.prefetchQuery(mailQueryKeys.page._ctx.all({ page, size }, search));
+  },
 });
