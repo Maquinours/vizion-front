fac4b670850420d41192734a0ed69080d2211903
Maquinours <juju9559@hotmail.fr>
2024-05-15 09:08:21 +0200

    collective tasks optimization
    use links instead of buttons

--- src/views/App/views/Dashboard/components/CollectiveTasks/components/Table/Table.tsx
+++ src/views/App/views/Dashboard/components/CollectiveTasks/components/Table/Table.tsx
@@ -1,13 +1,11 @@
 import { VirtualElement } from '@popperjs/core';
-import { useQueryClient } from '@tanstack/react-query';
-import { getRouteApi, useNavigate } from '@tanstack/react-router';
+import { Link, getRouteApi } from '@tanstack/react-router';
 import { Row, RowSelectionState, createColumnHelper } from '@tanstack/react-table';
 import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import IndeterminateCheckboxComponent from '../../../../../../../../components/IndeterminateCheckbox/IndeterminateCheckbox';
 import TableComponent from '../../../../../../../../components/Table/Table';
-import { queries } from '../../../../../../../../utils/constants/queryKeys';
 import { formatDateAndHourWithSlash } from '../../../../../../../../utils/functions/dates';
 import TaskResponseDto from '../../../../../../../../utils/types/TaskResponseDto';
 import styles from './Table.module.scss';
@@ -16,6 +14,49 @@ import ContextMenu from './components/ContextMenu/ContextMenu';
 const Route = getRouteApi('/app/dashboard');
 
 const columnHelper = createColumnHelper<TaskResponseDto>();
+
+const columns = [
+  columnHelper.display({
+    id: 'checker',
+    cell: ({ row }) => (
+      <IndeterminateCheckboxComponent
+        {...{
+          checked: row.getIsSelected(),
+          disabled: !row.getCanSelect(),
+          indeterminate: row.getIsSomeSelected(),
+          onChange: row.getToggleSelectedHandler(),
+        }}
+      />
+    ),
+  }),
+  columnHelper.display({
+    id: 'content',
+    cell: ({ row: { original } }) => (
+      <div className={styles.more_content}>
+        <div className={styles.content}>
+          {original.mailId ? (
+            <Link from={Route.id} to="task-email/$taskId" params={{ taskId: original.id }} search={(old) => old} className={styles.mail_content}>
+              <div>{parse(DOMPurify.sanitize(original.content ?? ''))}</div>
+              <p>
+                À : {original.receiver?.to?.toString()?.split(';').join(' ')} {original.receiver?.cc?.toString()}
+              </p>
+              <p>
+                De : <a href={`mailto:${original.name}`}>{original.name}</a>
+              </p>
+            </Link>
+          ) : (
+            <div className={styles.default_task}>
+              <button>{original.content}</button>
+              <div>
+                <span>{original.name}</span> <span>{formatDateAndHourWithSlash(original.createdDate)}</span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    ),
+  }),
+];
 
 type AppViewDashboardViewCollectiveTasksComponentTableComponentProps = Readonly<{
   data: Array<TaskResponseDto> | undefined;
@@ -29,70 +70,8 @@ export default function AppViewDashboardViewCollectiveTasksComponentTableCompone
   rowSelection,
   setRowSelection,
 }: AppViewDashboardViewCollectiveTasksComponentTableComponentProps) {
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-
   const [currentTask, setCurrentTask] = useState<TaskResponseDto>();
   const [contextMenuAnchorElement, setContextMenuAnchorElement] = useState<VirtualElement | undefined>(undefined);
-
-  const onRowContentClick = useCallback(
-    // TODO: remove this function and use link instead
-    (task: TaskResponseDto) => {
-      if (task.mailId) {
-        queryClient.setQueryData(queries.tasks.detail(task.id).queryKey, task);
-        navigate({ from: Route.id, to: 'task-email/$taskId', params: { taskId: task.id }, search: (old) => old });
-      } else if (task.businessId) {
-        // TODO: implement redirect to business
-      }
-    },
-    [navigate, queryClient],
-  );
-
-  const columns = useMemo(
-    () => [
-      columnHelper.display({
-        id: 'checker',
-        cell: ({ row }) => (
-          <IndeterminateCheckboxComponent
-            {...{
-              checked: row.getIsSelected(),
-              disabled: !row.getCanSelect(),
-              indeterminate: row.getIsSomeSelected(),
-              onChange: row.getToggleSelectedHandler(),
-            }}
-          />
-        ),
-      }),
-      columnHelper.display({
-        id: 'content',
-        cell: ({ row: { original } }) => (
-          <div className={styles.more_content}>
-            <div className={styles.content}>
-              {original.mailId ? (
-                <button className={styles.mail_content} onClick={() => onRowContentClick(original)}>
-                  <div>{parse(DOMPurify.sanitize(original.content ?? ''))}</div>
-                  <p>
-                    À : {original.receiver?.to?.toString()?.split(';').join(' ')} {original.receiver?.cc?.toString()}
-                  </p>
-                  <p>
-                    De : <a href={`mailto:${original.name}`}>{original.name}</a>
-                  </p>
-                </button>
-              ) : (
-                <div className={styles.default_task}>
-                  <button onClick={() => onRowContentClick(original)}>{original.content}</button>
-                  <div>
-                    <span>{original.name}</span> <span>{formatDateAndHourWithSlash(original.createdDate)}</span>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        ),
-      }),
-    ],
-    [onRowContentClick],
-  );
 
   const onTableRowContextMenu = (e: React.MouseEvent, row: Row<TaskResponseDto>) => {
     e.preventDefault();
