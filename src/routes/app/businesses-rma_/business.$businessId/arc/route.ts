e7e55c106fad951390c9a3939c7bf6ffe8625ae1
Maquinours <juju9559@hotmail.fr>
2024-05-17 11:56:01 +0200

    arc page loader

--- src/routes/app/businesses-rma_/business.$businessId/arc/route.ts
+++ src/routes/app/businesses-rma_/business.$businessId/arc/route.ts
@@ -1,5 +1,6 @@
 import { SearchSchemaInput, createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
+import { queries } from '../../../../../utils/constants/queryKeys';
 
 const searchSchema = z.object({
   hideReferencesPrices: z.boolean().catch(false),
@@ -7,4 +8,8 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/arc')({
   validateSearch: (data: { hideReferencesPrices?: boolean } & SearchSchemaInput) => searchSchema.parse(data),
+  loader: async ({ context: { queryClient }, params: { businessId } }) => {
+    queryClient.prefetchQuery(queries['product-stocks'].list._ctx.all);
+    await queryClient.ensureQueryData(queries['business-ARCs'].detail._ctx.byBusinessId(businessId));
+  },
 });
