8730822444d0e2449e18d364272fef6cbd9d77af
Maquinours <juju9559@hotmail.fr>
2024-05-17 11:56:40 +0200

    bl page loader

--- src/routes/app/businesses-rma_/business.$businessId/bl/route.ts
+++ src/routes/app/businesses-rma_/business.$businessId/bl/route.ts
@@ -1,5 +1,6 @@
 import { SearchSchemaInput, createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
+import { queries } from '../../../../../utils/constants/queryKeys';
 
 const searchSchema = z.object({
   page: z.number().min(0).catch(0),
@@ -7,4 +8,7 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/bl')({
   validateSearch: (data: { page?: number } & SearchSchemaInput) => searchSchema.parse(data),
+  loader: async ({ context: { queryClient }, params: { businessId } }) => {
+    await queryClient.ensureQueryData(queries['business-bls'].list._ctx.byBusinessId(businessId));
+  },
 });
