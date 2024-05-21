2f1d870d12328b76bf5e492e21813f2c095dac4f
Maquinours <juju9559@hotmail.fr>
2024-05-21 09:30:26 +0200

    dashboard subroutes loader optimizations

--- src/routes/app/dashboard/archive-personal-task.$taskId/route.ts
+++ src/routes/app/dashboard/archive-personal-task.$taskId/route.ts
@@ -1,6 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { queries } from '../../../../utils/constants/queryKeys';
+import { QueryKey } from '@tanstack/react-query';
+import Page from '../../../../utils/types/Page';
+import TaskResponseDto from '../../../../utils/types/TaskResponseDto';
 
 export const Route = createFileRoute('/app/dashboard/archive-personal-task/$taskId')({
-  loader: ({ context: { queryClient }, params: { taskId } }) => queryClient.ensureQueryData(queries.tasks.detail(taskId)),
+  loader: async ({ context: { queryClient }, params: { taskId } }) => {
+    let initialDataKey: QueryKey | undefined = undefined;
+    await queryClient.ensureQueryData({
+      ...queries.tasks.detail(taskId),
+      initialData: () => {
+        for (const [key, value] of queryClient.getQueriesData<Page<TaskResponseDto>>({ queryKey: queries.tasks.page.queryKey })) {
+          const item = value?.content.find((item) => item.id === taskId);
+          if (item) {
+            initialDataKey = key;
+            return item;
+          }
+        }
+      },
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
+    });
+  },
 });
