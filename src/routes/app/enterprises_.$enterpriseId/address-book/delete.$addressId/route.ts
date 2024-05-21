9d4b2c16e210c455544009cb55085e194a065795
Maquinours <juju9559@hotmail.fr>
2024-05-21 11:58:04 +0200

    enterprise address book subroutes loader optimize

--- src/routes/app/enterprises_.$enterpriseId/address-book/delete.$addressId/route.ts
+++ src/routes/app/enterprises_.$enterpriseId/address-book/delete.$addressId/route.ts
@@ -1,8 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { queries } from '../../../../../utils/constants/queryKeys';
+import Page from '../../../../../utils/types/Page';
+import AddressResponseDto from '../../../../../utils/types/AddressResponseDto';
+import { QueryKey } from '@tanstack/react-query';
 
 export const Route = createFileRoute('/app/enterprises/$enterpriseId/address-book/delete/$addressId')({
   loader: async ({ context: { queryClient }, params: { addressId } }) => {
-    await queryClient.ensureQueryData(queries.address.detail._ctx.byId(addressId));
+    let initialDataKey: QueryKey | undefined = undefined;
+    await queryClient.ensureQueryData({
+      ...queries.address.detail._ctx.byId(addressId),
+      initialData: () => {
+        for (const [key, value] of queryClient.getQueriesData<Page<AddressResponseDto>>({ queryKey: queries.address.page.queryKey })) {
+          const item = value?.content.find((item) => item.id === addressId);
+          if (item) {
+            initialDataKey = key;
+            return item;
+          }
+        }
+      },
+      initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
+    });
   },
 });
