98212dfd1cdfafe5325bc0de74b1fcdbe3faf2f8
Maquinours <juju9559@hotmail.fr>
2024-05-23 10:12:02 +0200

    App Sidebar & Footer optimizations

--- src/views/App/components/Sidebar/components/BasicSidebar/BasicSidebar.tsx
+++ src/views/App/components/Sidebar/components/BasicSidebar/BasicSidebar.tsx
@@ -5,95 +5,97 @@ import { useAuthentifiedUserQuery } from '../../../../utils/functions/getAuthent
 import React, { useMemo } from 'react';
 import { IconType } from 'react-icons/lib';
 import { IoMdHome } from 'react-icons/io';
+import ProfileInfoResponseDto from '../../../../../../utils/types/ProfileInfoResponseDto';
 
 type MenuItem = {
   icon: IconType;
   label: string;
-  route: LinkProps;
+  route: LinkProps | ((authentifiedUser: ProfileInfoResponseDto) => LinkProps);
   allowedRoles?: string[];
 };
+
+const MENUS: Array<MenuItem> = [
+  {
+    icon: MdBusinessCenter,
+    label: 'Nouvelle affaire',
+    route: {
+      params: (old) => old,
+      search: (old) => ({ ...old, appModal: 'create-client-business', businessId: undefined, gedItemKey: undefined }),
+    },
+    allowedRoles: ['ROLE_DISTRIBUTEUR', 'ROLE_CLIENT'],
+  },
+  {
+    icon: IoMdHome,
+    label: 'Tableau de bord',
+    route: {
+      to: '/app/dashboard',
+    },
+    allowedRoles: ['ROLE_MEMBRE_VIZEO'],
+  },
+  {
+    icon: MdBusinessCenter,
+    label: 'Affaires',
+    route: {
+      to: '/app/businesses-rma',
+    },
+  },
+  {
+    icon: MdGroup,
+    label: 'Mon entreprise',
+    route: (authentifiedUser) => ({
+      to: '/app/enterprises/$enterpriseId',
+      params: { entepriseId: authentifiedUser.profile.enterprise!.id },
+    }),
+    allowedRoles: ['ROLE_CLIENT'],
+  },
+  {
+    icon: MdSell,
+    label: 'Produits',
+    route: {
+      to: '/app/products',
+    },
+  },
+  {
+    icon: MdGroups,
+    label: 'Entreprises',
+    route: {
+      to: '/app/enterprises',
+    },
+    allowedRoles: ['ROLE_MEMBRE_VIZEO', 'ROLE_REPRESENTANT', 'ROLE_DISTRIBUTEUR'],
+  },
+  {
+    icon: MdQuestionAnswer,
+    label: 'FAQ',
+    route: {
+      to: '/app/faq',
+    },
+  },
+  {
+    icon: MdLeakAdd,
+    label: 'Liens externes',
+    route: {
+      to: '/app/external-links',
+    },
+  },
+  {
+    icon: MdBuild,
+    label: 'Outils',
+    route: {
+      to: '/app/tools/menu',
+    },
+    allowedRoles: ['ROLE_MEMBRE_VIZEO'],
+  },
+];
 
 export default function SidebarComponentBasicSidebarComponent() {
   const { data: authentifiedUser } = useAuthentifiedUserQuery();
 
-  const MENUS: MenuItem[] = useMemo(
-    () => [
-      {
-        icon: MdBusinessCenter,
-        label: 'Nouvelle affaire',
-        route: {
-          params: (old) => old,
-          search: (old) => ({ ...old, appModal: 'create-client-business', businessId: undefined, gedItemKey: undefined }),
-        },
-        allowedRoles: ['ROLE_DISTRIBUTEUR', 'ROLE_CLIENT'],
-      },
-      {
-        icon: IoMdHome,
-        label: 'Tableau de bord',
-        route: {
-          to: '/app/dashboard',
-        },
-        allowedRoles: ['ROLE_MEMBRE_VIZEO'],
-      },
-      {
-        icon: MdBusinessCenter,
-        label: 'Affaires',
-        route: {
-          to: '/app/businesses-rma',
-        },
-      },
-      {
-        icon: MdGroup,
-        label: 'Mon entreprise',
-        route: {
-          to: '/app/enterprises/$enterpriseId',
-          params: { entepriseId: authentifiedUser.profile.enterprise!.id },
-        },
-        allowedRoles: ['ROLE_CLIENT'],
-      },
-      {
-        icon: MdSell,
-        label: 'Produits',
-        route: {
-          to: '/app/products',
-        },
-      },
-      {
-        icon: MdGroups,
-        label: 'Entreprises',
-        route: {
-          to: '/app/enterprises',
-        },
-        allowedRoles: ['ROLE_MEMBRE_VIZEO', 'ROLE_REPRESENTANT', 'ROLE_DISTRIBUTEUR'],
-      },
-      {
-        icon: MdQuestionAnswer,
-        label: 'FAQ',
-        route: {
-          to: '/app/faq',
-        },
-      },
-      {
-        icon: MdLeakAdd,
-        label: 'Liens externes',
-        route: {
-          to: '/app/external-links',
-        },
-      },
-      {
-        icon: MdBuild,
-        label: 'Outils',
-        route: {
-          to: '/app/tools/menu',
-        },
-        allowedRoles: ['ROLE_MEMBRE_VIZEO'],
-      },
-    ],
-    [authentifiedUser.profile.enterprise],
-  );
-
   const menus = useMemo(
-    () => MENUS.filter((menu) => !menu.allowedRoles || menu.allowedRoles.some((role) => authentifiedUser.userInfo.roles.includes(role))),
+    () =>
+      MENUS.filter((menu) => !menu.allowedRoles || menu.allowedRoles.some((role) => authentifiedUser.userInfo.roles.includes(role))).map((menu) => ({
+        ...menu,
+        route: typeof menu.route === 'function' ? menu.route(authentifiedUser) : menu.route,
+      })),
     [MENUS, authentifiedUser.userInfo.roles],
   );
 
