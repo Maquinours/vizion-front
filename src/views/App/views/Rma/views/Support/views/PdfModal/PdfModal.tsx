ccd6cec31aba8090121c5f30c5ff5ea140e0f54c
Maquinours <juju9559@hotmail.fr>
2024-06-03 16:22:19 +0200

    RMA support pdf add missing outlet

--- src/views/App/views/Rma/views/Support/views/PdfModal/PdfModal.tsx
+++ src/views/App/views/Rma/views/Support/views/PdfModal/PdfModal.tsx
@@ -2,7 +2,7 @@ import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import ReactModal from 'react-modal';
 import styles from './PdfModal.module.scss';
 import AppViewRmaViewSupportViewPdfModalViewPdfComponent from './components/Pdf/Pdf';
-import { Link, getRouteApi, useNavigate } from '@tanstack/react-router';
+import { Link, Outlet, getRouteApi, useNavigate } from '@tanstack/react-router';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { queries } from '../../../../../../../../utils/constants/queryKeys';
 import { useAuthentifiedUserQuery } from '../../../../../../utils/functions/getAuthentifiedUser';
@@ -23,30 +23,33 @@ export default function AppViewRmaViewSupportViewPdfModalView() {
   };
 
   return (
-    <ReactModal isOpen={true} onRequestClose={onClose} className={styles.modal} overlayClassName="Overlay">
-      <div className={styles.modal_container}>
-        <div className={styles.modal_title}>
-          <h6>Votre RMA :</h6>
+    <>
+      <ReactModal isOpen={true} onRequestClose={onClose} className={styles.modal} overlayClassName="Overlay">
+        <div className={styles.modal_container}>
+          <div className={styles.modal_title}>
+            <h6>Votre RMA :</h6>
+          </div>
+          <div className={styles.modal_pdfviewer}>
+            <PDFViewer>
+              <AppViewRmaViewSupportViewPdfModalViewPdfComponent rma={rma} />
+            </PDFViewer>
+          </div>
+          <div className={styles.modal_footer}>
+            <button className="btn btn-primary" onClick={() => onClose()}>
+              Modifier
+            </button>
+            <PDFDownloadLink document={<AppViewRmaViewSupportViewPdfModalViewPdfComponent rma={rma} />} fileName={`RMA_${rma.number}.pdf`}>
+              {({ loading }) => <button className="btn btn-secondary">{loading ? 'Chargement...' : 'Télécharger'}</button>}
+            </PDFDownloadLink>
+            {authentifiedUser.userInfo.roles.includes('ROLE_MEMBRE_VIZEO') && (
+              <Link from={routeApi.id} to="send-by-email" search replace resetScroll={false} className="btn btn-secondary">
+                Envoyer par mail
+              </Link>
+            )}
+          </div>
         </div>
-        <div className={styles.modal_pdfviewer}>
-          <PDFViewer>
-            <AppViewRmaViewSupportViewPdfModalViewPdfComponent rma={rma} />
-          </PDFViewer>
-        </div>
-        <div className={styles.modal_footer}>
-          <button className="btn btn-primary" onClick={() => onClose()}>
-            Modifier
-          </button>
-          <PDFDownloadLink document={<AppViewRmaViewSupportViewPdfModalViewPdfComponent rma={rma} />} fileName={`RMA_${rma.number}.pdf`}>
-            {({ loading }) => <button className="btn btn-secondary">{loading ? 'Chargement...' : 'Télécharger'}</button>}
-          </PDFDownloadLink>
-          {authentifiedUser.userInfo.roles.includes('ROLE_MEMBRE_VIZEO') && (
-            <Link from={routeApi.id} to="send-by-email" search replace resetScroll={false} className="btn btn-secondary">
-              Envoyer par mail
-            </Link>
-          )}
-        </div>
-      </div>
-    </ReactModal>
+      </ReactModal>
+      <Outlet />
+    </>
   );
 }
