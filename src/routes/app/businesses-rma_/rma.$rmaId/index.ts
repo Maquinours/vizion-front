8aa98d09b7437f49c830616d19b67e40d7a43397
Maquinours <juju9559@hotmail.fr>
2024-06-04 08:54:19 +0200

    RMA access context undefined temporary fix

--- src/routes/app/businesses-rma_/rma.$rmaId/index.ts
+++ src/routes/app/businesses-rma_/rma.$rmaId/index.ts
@@ -3,7 +3,8 @@ import { queries } from '../../../../utils/constants/queryKeys';
 import AssistanceState from '../../../../utils/enums/AssistanceState';
 
 export const Route = createFileRoute('/app/businesses-rma/rma/$rmaId/')({
-  beforeLoad: async ({ context: { queryClient }, params: { rmaId } }) => {
+  // TODO: set this code to beforeLoad instead when route context undefined error is fixed
+  loader: async ({ context: { queryClient }, params: { rmaId } }) => {
     const rma = await queryClient.ensureQueryData(queries.rmas.detail(rmaId));
     switch (rma.state) {
       case AssistanceState.RECEPTION:
