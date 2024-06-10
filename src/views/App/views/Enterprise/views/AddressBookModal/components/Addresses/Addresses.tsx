110ac50546ade166e9ac9bf4793fc47d6d430372
Maquinours <juju9559@hotmail.fr>
2024-06-10 13:40:03 +0200

    AddressBook fixes

--- src/views/App/views/Enterprise/views/AddressBookModal/components/Addresses/Addresses.tsx
+++ src/views/App/views/Enterprise/views/AddressBookModal/components/Addresses/Addresses.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
 import { PulseLoader } from 'react-spinners';
+import AddressResponseDto from '../../../../../../../../utils/types/AddressResponseDto';
 import styles from './Addresses.module.scss';
 import EnterpriseAddressBookModalAddress from './components/Address/Address';
-import { VirtualElement } from '@popperjs/core';
-import EnterpriseAddressBookModalAddressContextMenu from './components/AddressContextMenu/AddressContextMenu';
-import AddressResponseDto from '../../../../../../../../utils/types/AddressResponseDto';
 
 type AppViewEnterpriseViewAddressBookModalViewAddressesComponentProps = Readonly<{
   isLoading: boolean;
@@ -14,27 +11,6 @@ export default function AppViewEnterpriseViewAddressBookModalViewAddressesCompon
   isLoading,
   addresses,
 }: AppViewEnterpriseViewAddressBookModalViewAddressesComponentProps) {
-  const [contextMenuAnchorElement, setContextMenuAnchorElement] = useState<VirtualElement>();
-  const [address, setAddress] = useState<AddressResponseDto>();
-
-  const onAddressContextMenu = (e: React.MouseEvent, address: AddressResponseDto) => {
-    e.preventDefault();
-    setAddress(address);
-    setContextMenuAnchorElement({
-      getBoundingClientRect: () => ({
-        width: 0,
-        height: 0,
-        x: e.clientX,
-        y: e.clientY,
-        top: e.clientY,
-        right: e.clientX,
-        bottom: e.clientY,
-        left: e.clientX,
-        toJSON: () => {},
-      }),
-    });
-  };
-
   if (isLoading) {
     return (
       <div className={styles.loading}>
@@ -45,13 +21,10 @@ export default function AppViewEnterpriseViewAddressBookModalViewAddressesCompon
     return <div className={styles.no_content}>Aucune adresse enregistrée</div>;
   }
   return (
-    <>
-      <div className={styles.address_container}>
-        {addresses.map((address, index) => (
-          <EnterpriseAddressBookModalAddress key={address.id} address={address} index={index} onContextMenu={onAddressContextMenu} />
-        ))}
-      </div>
-      <EnterpriseAddressBookModalAddressContextMenu anchorElement={contextMenuAnchorElement} setAnchorElement={setContextMenuAnchorElement} address={address} />
-    </>
+    <div className={styles.address_container}>
+      {addresses.map((address, index) => (
+        <EnterpriseAddressBookModalAddress key={address.id} address={address} index={index} />
+      ))}
+    </div>
   );
 }
