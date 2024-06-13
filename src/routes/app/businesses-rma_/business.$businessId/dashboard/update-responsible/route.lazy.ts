02ef3faed1f33109c0d406e193691098d0aef4e4
Maquinours <juju9559@hotmail.fr>
2024-06-13 08:56:05 +0200

    routes lazy fixes

--- src/routes/app/businesses-rma_/business.$businessId/dashboard/update-responsible/route.lazy.ts
+++ src/routes/app/businesses-rma_/business.$businessId/dashboard/update-responsible/route.lazy.ts
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createLazyFileRoute } from '@tanstack/react-router';
 import AppViewBusinessViewDashboardViewUpdateResponsibleModalView from '../../../../../../views/App/views/Business/views/Dashboard/views/UpdateResponsibleModal/UpdateResponsibleModal';
 
-export const Route = createFileRoute('/app/businesses-rma/business/$businessId/dashboard/update-responsible')({
+export const Route = createLazyFileRoute('/app/businesses-rma/business/$businessId/dashboard/update-responsible')({
   component: AppViewBusinessViewDashboardViewUpdateResponsibleModalView,
 });
