4600d42f0965c9e441d20823021d1e4a1383c755
Maquinours <juju9559@hotmail.fr>
2024-06-14 10:25:55 +0200

    dashboard loading & context undefined fix

--- src/routes/app/dashboard/route.ts
+++ src/routes/app/dashboard/route.ts
@@ -1,11 +1,11 @@
 import { SearchSchemaInput, createFileRoute, redirect } from '@tanstack/react-router';
 import { Views } from 'react-big-calendar';
 import { z } from 'zod';
-// import { queries } from '../../../utils/constants/queryKeys';
-// import { keycloakEvents } from '../../../utils/constants/queryKeys/keycloakEvent';
+import { queries } from '../../../utils/constants/queryKeys';
+import { keycloakEvents } from '../../../utils/constants/queryKeys/keycloakEvent';
 import { users } from '../../../utils/constants/queryKeys/user';
 import TaskState from '../../../utils/enums/TaskState';
-// import WorkloadType from '../../../utils/enums/WorkloadType';
+import WorkloadType from '../../../utils/enums/WorkloadType';
 
 const searchSchema = z.object({
   personalTaskState: z.nativeEnum(TaskState).catch(TaskState.CREATED),
@@ -18,24 +18,19 @@ export const Route = createFileRoute('/app/dashboard')({
   validateSearch: (
     data: { personalTaskState?: TaskState; personalTaskPage?: number; schedulerView?: 'day' | 'work_week'; schedulerDate?: Date } & SearchSchemaInput,
   ) => searchSchema.parse(data),
-  beforeLoad: async ({ context: { queryClient } }) => {
+  loaderDeps: ({ search: { personalTaskState, personalTaskPage } }) => ({ personalTaskState, personalTaskPage, personalTaskSize: 10 }),
+  loader: async ({ context: { queryClient }, deps: { personalTaskState, personalTaskPage, personalTaskSize } }) => {
     const user = await queryClient.ensureQueryData(users.authentified());
-    if (!user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO')) {
-      console.warn('User is not allowed to access this route', user, Route);
-      throw redirect({ to: '..' });
-    }
+    if (!user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO')) throw redirect({ from: Route.id, to: '..' });
+
+    queryClient.prefetchQuery(keycloakEvents.page({ page: 0, size: 100 }));
+    queryClient.prefetchQuery(queries.tasks.list._ctx.byType(WorkloadType.COLLECTIVE));
+    queryClient.prefetchQuery(queries['rdv-user-infos'].list);
+    queryClient.prefetchQuery(queries['progressive-infos'].list);
+    queryClient.prefetchQuery(
+      queries.tasks.page._ctx.byStateAndProfileId(personalTaskState, user.profile.id, { page: personalTaskPage, size: personalTaskSize }),
+    );
   },
-  // loaderDeps: ({ search: { personalTaskState, personalTaskPage } }) => ({ personalTaskState, personalTaskPage, personalTaskSize: 10 }),
-  // loader: async ({ context: { queryClient }, deps: { personalTaskState, personalTaskPage, personalTaskSize } }) => {
-  //   queryClient.prefetchQuery(keycloakEvents.page({ page: 0, size: 100 }));
-  //   queryClient.prefetchQuery(queries.tasks.list._ctx.byType(WorkloadType.COLLECTIVE));
-  //   queryClient.prefetchQuery(queries['rdv-user-infos'].list);
-  //   queryClient.prefetchQuery(queries['progressive-infos'].list);
-  //   const user = await queryClient.ensureQueryData(users.authentified());
-  //   queryClient.prefetchQuery(
-  //     queries.tasks.page._ctx.byStateAndProfileId(personalTaskState, user.profile.id, { page: personalTaskPage, size: personalTaskSize }),
-  //   );
-  // },
   staticData: {
     title: 'Tableau de bord',
   },
