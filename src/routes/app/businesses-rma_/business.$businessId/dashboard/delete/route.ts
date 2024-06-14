f287cca4fb26e8f13d793d5c98765e4bd871d7ce
Maquinours <juju9559@hotmail.fr>
2024-06-14 10:27:43 +0200

    add route securities

--- src/routes/app/businesses-rma_/business.$businessId/dashboard/delete/route.ts
+++ src/routes/app/businesses-rma_/business.$businessId/dashboard/delete/route.ts
@@ -1,6 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import LoaderModal from '../../../../../../components/LoaderModal/LoaderModal';
+import { queries } from '../../../../../../utils/constants/queryKeys';
+import BusinessState from '../../../../../../utils/enums/BusinessState';
 
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/dashboard/delete')({
+  loader: async ({ context: { queryClient }, params: { businessId } }) => {
+    const business = await queryClient.ensureQueryData(queries.businesses.detail._ctx.byId(businessId));
+    if (!((business.state === BusinessState.DEVIS || business.state === BusinessState.CREATED) && !business.archived))
+      throw redirect({ from: Route.id, to: '..', search: true, replace: true });
+  },
   pendingComponent: LoaderModal,
 });
--- src/routes/app/businesses-rma_/business.$businessId/dashboard/update-representative/route.ts
+++ src/routes/app/businesses-rma_/business.$businessId/dashboard/update-representative/route.ts
@@ -4,12 +4,10 @@ import CategoryClient from '../../../../../../utils/enums/CategoryClient';
 import LoaderModal from '../../../../../../components/LoaderModal/LoaderModal';
 
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/dashboard/update-representative')({
-  beforeLoad: async ({ context: { queryClient } }) => {
-    const user = await queryClient.ensureQueryData(queries.user.authentified());
-    if (!user.userInfo.roles.includes('ROLE_DIRECTION_VIZEO')) throw redirect({ from: Route.id, to: '..', search: (old) => old, replace: true });
-  },
   loader: async ({ context: { queryClient } }) => {
     queryClient.prefetchQuery(queries.enterprise.list._ctx.byCategory(CategoryClient.REPRESENTANT));
+    const user = await queryClient.ensureQueryData(queries.user.authentified());
+    if (!user.userInfo.roles.includes('ROLE_DIRECTION_VIZEO')) throw redirect({ from: Route.id, to: '..', search: (old) => old, replace: true });
   },
   pendingComponent: LoaderModal,
 });
