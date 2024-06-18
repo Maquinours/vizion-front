112cb302bab620a46148b2e0e9bca84858aaf0f5
Maquinours <juju9559@hotmail.fr>
2024-06-18 16:03:49 +0200

    Chat websocket handle API change

--- src/views/App/components/ChatWebsocket/ChatWebsocket.tsx
+++ src/views/App/components/ChatWebsocket/ChatWebsocket.tsx
@@ -8,29 +8,69 @@ import { useAuthentifiedUserQuery } from '../../utils/functions/getAuthentifiedU
 import { useEffect, useState } from 'react';
 
 type MessageData = {
-  function: 'new-message' | 'message-sent';
+  function: 'new-messages' | 'message-sent';
 };
 
-type NewMessageMessageData = {
-  function: 'new-message';
-  data: Array<{
-    details: {
-      attachments: string;
-      creation_time: string;
-      first_name: string;
-      full_name: string;
-      id: string;
-      last_name: string;
-      message: string;
-      profile_image: string;
-      status_code: string;
-      user_id: string;
-      user_type: string;
-    };
-  }>;
+type NewMessagesMessageData = {
+  function: 'new-messages';
+  data: {
+    messages: Array<{
+      details: {
+        id: string;
+        user_id: string;
+        message: string;
+        creation_time: string;
+        status_code: string;
+        attachments: string;
+        first_name: string;
+        last_name: string;
+        profile_image: string;
+        user_type: string;
+        full_name: string;
+      };
+    }>;
+  };
 };
 
-const isNewMessage = (data: MessageData): data is NewMessageMessageData => data.function === 'new-message';
+const isNewMessagesMessageData = (data: MessageData): data is NewMessagesMessageData => {
+  return (
+    data.function === 'new-messages' &&
+    'data' in data &&
+    !!data.data &&
+    typeof data.data === 'object' &&
+    'messages' in data.data &&
+    Array.isArray(data.data.messages) &&
+    data.data.messages.every(
+      (message) =>
+        !!message &&
+        typeof message === 'object' &&
+        'details' in message &&
+        typeof message.details === 'object' &&
+        'id' in message.details &&
+        typeof message.details.id === 'string' &&
+        'user_id' in message.details &&
+        typeof message.details.user_id === 'string' &&
+        'message' in message.details &&
+        typeof message.details.message === 'string' &&
+        'creation_time' in message.details &&
+        typeof message.details.creation_time === 'string' &&
+        'status_code' in message.details &&
+        typeof message.details.status_code === 'string' &&
+        'attachments' in message.details &&
+        typeof message.details.attachments === 'string' &&
+        'first_name' in message.details &&
+        typeof message.details.first_name === 'string' &&
+        'last_name' in message.details &&
+        typeof message.details.last_name === 'string' &&
+        'profile_image' in message.details &&
+        typeof message.details.profile_image === 'string' &&
+        'user_type' in message.details &&
+        typeof message.details.user_type === 'string' &&
+        'full_name' in message.details &&
+        typeof message.details.full_name === 'string',
+    )
+  );
+};
 
 export default function AppViewChatWebsocketComponent() {
   const navigate = useNavigate();
@@ -63,13 +103,19 @@ export default function AppViewChatWebsocketComponent() {
       onMessage: (message) => {
         try {
           const data = JSON.parse(message.data) as MessageData;
-          if (isNewMessage(data) && data.data.length === 1 && !['bot', 'admin', 'agent'].includes(data.data[0].details.user_type)) {
+          if (
+            isNewMessagesMessageData(data) &&
+            data.data.messages.length === 1 &&
+            !['bot', 'admin', 'agent'].includes(data.data.messages[0].details.user_type)
+          ) {
             let toastMessage = 'Un utilisateur a envoyé un nouveau message sur le chat VIZEO';
-            if (data.data[0].details.message.length > 0)
+            if (data.data.messages[0].details.message.length > 0)
               toastMessage += ` :\n${
-                data.data[0].details.message.length > 150 ? data.data[0].details.message.substring(0, 150) + '...' : data.data[0].details.message
+                data.data.messages[0].details.message.length > 150
+                  ? data.data.messages[0].details.message.substring(0, 150) + '...'
+                  : data.data.messages[0].details.message
               }`;
-            else if (data.data[0].details.attachments.length > 0) toastMessage += ' contenant une ou plusieurs pièces jointes.';
+            else if (data.data.messages[0].details.attachments.length > 0) toastMessage += ' contenant une ou plusieurs pièces jointes.';
             else {
               console.error('New message does not include message and content.', data);
               return;
