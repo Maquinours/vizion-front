6d3724d7ae167bc76351b9e248e44a1df67c02d5
Maquinours <juju9559@hotmail.fr>
2024-08-01 15:37:35 +0200

    synopticCamera quantity amount format

--- src/views/App/views/Study/views/Expert/components/Flow/components/SynopticCameraNode/SynopticCameraNode.tsx
+++ src/views/App/views/Study/views/Expert/components/Flow/components/SynopticCameraNode/SynopticCameraNode.tsx
@@ -4,6 +4,7 @@ import { Handle, Node, NodeProps, NodeResizer, OnResize, Position, useReactFlow 
 import React, { useState } from 'react';
 import { queries } from '../../../../../../../../../../utils/constants/queryKeys';
 import AppViewStudyViewExpertViewFlowComponentSynopticCameraNodeComponentMenuComponent from './components/Menu/Menu';
+import AmountFormat from '../../../../../../../../../../components/AmountFormat/AmountFormat';
 
 export const isExpertStudySynopticCameraNode = (node: Node): node is ExpertStudySynopticCameraNode => {
   return (
@@ -119,9 +120,12 @@ export default function AppViewStudyViewExpertViewFlowComponentSynopticCameraNod
               </div>
               <div className="my-auto ml-auto">
                 {!!data.quantity && data.quantity > 1 && (
-                  <p className="absolute right-1 top-[calc(50%-30px)] ml-auto h-fit w-fit rounded-md bg-amber-300 p-[1px] text-center text-sm font-medium text-white">
-                    x{data.quantity}
-                  </p>
+                  <AmountFormat
+                    prefix="x"
+                    value={data.quantity}
+                    displayType="text"
+                    className="absolute right-1 top-[calc(50%-30px)] ml-auto h-fit w-fit rounded-md bg-amber-300 p-[1px] text-center text-sm font-medium text-white"
+                  />
                 )}
                 <p className="h-4 text-sm">{name}</p>
               </div>
