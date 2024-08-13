55d2308775ac19096f3c7c42e879eb2526ed3bac
Maquinours <juju9559@hotmail.fr>
2024-08-13 13:33:51 +0200

    missing loader added

--- src/routes/app/businesses-rma_/business.$businessId/quotation/pdf/route.ts
+++ src/routes/app/businesses-rma_/business.$businessId/quotation/pdf/route.ts
@@ -1,6 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router';
 import LoaderModal from '../../../../../../components/LoaderModal/LoaderModal';
+import { queries } from '../../../../../../utils/constants/queryKeys';
 
 export const Route = createFileRoute('/app/businesses-rma/business/$businessId/quotation/pdf')({
+  loader: async ({ context: { queryClient }, params: { businessId } }) => {
+    const quotation = await queryClient.ensureQueryData(queries['business-quotations'].detail._ctx.byBusinessId(businessId));
+    const references = (quotation.subQuotationList
+      ?.flatMap((subQuotation) => subQuotation.quotationDetails?.map((detail) => detail.productReference))
+      ?.filter((reference) => reference !== null) ?? []) as string[];
+    await queryClient.ensureQueryData({
+      ...queries['commercial-notices'].data._ctx.byProductReferences(references),
+      staleTime: Infinity,
+    });
+  },
   pendingComponent: LoaderModal,
 });
