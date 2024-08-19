71f4bbae83d82df472a1ab1da44f61d00e23bfa0
Maquinours <juju9559@hotmail.fr>
2024-08-19 11:21:16 +0200

    send expert study warning

--- src/views/App/views/Study/views/Expert/components/ModalProvider/components/SendStudyModal/SendStudyModal.tsx
+++ src/views/App/views/Study/views/Expert/components/ModalProvider/components/SendStudyModal/SendStudyModal.tsx
@@ -3,20 +3,44 @@ import EnterpriseResponseDto from '../../../../../../../../../../utils/types/Ent
 import ExpertStudyContext from '../../../../utils/context';
 import AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentImageGenerationStepComponent from './components/GenerationStep/GenerationStep';
 import AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentShowStepComponent from './components/ShowStep/ShowStep';
+import useStore, { RFState } from '../../../Flow/utils/store';
+import { useShallow } from 'zustand/react/shallow';
+import AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentWarningStepComponent from './components/WarningStep/WarningStep';
+
+const selector = (state: RFState) => ({
+  getPages: state.getPages,
+});
 
 export default function AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponent() {
+  const { getPages } = useStore(useShallow(selector));
+
   const { setModal } = useContext(ExpertStudyContext)!;
 
   const [stepData, setStepData] = useState<
+    | { step: 'WARNING' }
     | { step: 'GENERATION' }
     | { step: 'SHOW'; data: { quotationPdf: File; studyPdf: File; commercialNoticePdf: File | null; representative: EnterpriseResponseDto | undefined } }
-  >({ step: 'GENERATION' });
+  >(() => {
+    const nodes = getPages()
+      .filter((page) => page.type === 'synoptic')
+      .flatMap((page) => page.nodes);
+    return nodes.some((node) => node.type === 'image' && node.data.image !== 'https://bd.vizeo.eu/6-Photos/BOX/Box.png')
+      ? { step: 'WARNING' }
+      : { step: 'GENERATION' };
+  });
 
   const onClose = () => {
     setModal(undefined);
   };
 
   switch (stepData.step) {
+    case 'WARNING':
+      return (
+        <AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentWarningStepComponent
+          onClose={onClose}
+          onConfirm={() => setStepData({ step: 'GENERATION' })}
+        />
+      );
     case 'GENERATION':
       return (
         <AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentImageGenerationStepComponent
---
+++ src/views/App/views/Study/views/Expert/components/ModalProvider/components/SendStudyModal/components/WarningStep/WarningStep.tsx
@@ -0,0 +1,39 @@
+import ReactModal from 'react-modal';
+
+type AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentWarningStepComponentProps = Readonly<{
+  onConfirm: () => void;
+  onClose: () => void;
+}>;
+export default function AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentWarningStepComponent({
+  onConfirm,
+  onClose,
+}: AppViewStudyViewExpertViewModalProviderComponentSendStudyModalComponentWarningStepComponentProps) {
+  return (
+    <ReactModal
+      isOpen
+      onRequestClose={onClose}
+      className="absolute left-2/4 top-2/4 m-auto h-auto w-auto min-w-72 -translate-x-2/4 -translate-y-2/4 rounded-md opacity-100"
+      overlayClassName="Overlay"
+    >
+      <div className="w-full rounded-md bg-white pb-2">
+        <h2 className="flex h-10 items-center justify-center rounded-t-md bg-[var(--primary-color)] text-white">Attention</h2>
+        <div className="flex flex-col gap-y-4 p-4">
+          <p className="text-center text-[var(--primary-color)]">
+            Des produits importés ne seront pas valorisés dans le chiffrage.
+            <br />
+            Voulez-vous continuer ?
+          </p>
+
+          <div className="flex items-center justify-center space-x-2">
+            <button type="button" onClick={onClose} className="btn btn-secondary">
+              Annuler
+            </button>
+            <button type="button" onClick={onConfirm} className="btn btn-primary">
+              Continuer
+            </button>
+          </div>
+        </div>
+      </div>
+    </ReactModal>
+  );
+}
