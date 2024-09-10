ebf1837ea86fb4b59269b6f340746fd619c0c5c4
Maquinours <juju9559@hotmail.fr>
2024-09-10 15:44:37 +0200

    expert study send quotation fix

--- src/views/App/views/Study/views/Expert/components/ModalProvider/components/SendStudyModal/components/GenerationStep/GenerationStep.tsx
+++ src/views/App/views/Study/views/Expert/components/ModalProvider/components/SendStudyModal/components/GenerationStep/GenerationStep.tsx
@@ -188,26 +188,29 @@ export default function AppViewStudyViewExpertViewModalProviderComponentSendStud
         .map(([groupName, products], index) => ({
           name: groupName,
           orderNum: `${index}`,
-          quotationDetails: products.map((product) => {
-            const publicUnitPrice = product.product.publicPrice ?? 0;
-            const unitPrice = Number((publicUnitPrice - publicUnitPrice * ((business.reduction ?? 0) / 100)).toFixed(2));
-            const totalPrice = Number((unitPrice * product.quantity).toFixed(2));
-            return {
-              groupName: groupName,
-              productId: product.product.id,
-              productName: product.product.reference,
-              productReference: product.product.reference ?? '',
-              productDesignation: product.product.shortDescription,
-              quantity: product.quantity,
-              reduction: business.reduction ?? 0,
-              taxDEEE: 0,
-              publicUnitPrice: publicUnitPrice,
-              unitPrice: unitPrice,
-              totalPrice: totalPrice,
-              virtualQty: product.product.virtualQty,
-            };
-          }),
-        }));
+          quotationDetails: products
+            .filter((product) => product.quantity > 0)
+            .map((product) => {
+              const publicUnitPrice = product.product.publicPrice ?? 0;
+              const unitPrice = Number((publicUnitPrice - publicUnitPrice * ((business.reduction ?? 0) / 100)).toFixed(2));
+              const totalPrice = Number((unitPrice * product.quantity).toFixed(2));
+              return {
+                groupName: groupName,
+                productId: product.product.id,
+                productName: product.product.reference,
+                productReference: product.product.reference ?? '',
+                productDesignation: product.product.shortDescription,
+                quantity: product.quantity,
+                reduction: business.reduction ?? 0,
+                taxDEEE: 0,
+                publicUnitPrice: publicUnitPrice,
+                unitPrice: unitPrice,
+                totalPrice: totalPrice,
+                virtualQty: product.product.virtualQty,
+              };
+            }),
+        }))
+        .filter(({ quotationDetails }) => quotationDetails.length > 0);
 
       const totalAmountHT = subQuotations.reduce(
         (acc, subQuotation) => acc + (subQuotation.quotationDetails?.reduce((acc, detail) => acc + detail.totalPrice, 0) ?? 0),
