7ca2edba3d723bdb1cef4b0721fa49ed07f7a5a9
Maquinours <juju9559@hotmail.fr>
2024-09-24 11:33:37 +0200

    sentry capture product cache error

--- src/routes/app/products_.$productId/route.ts
+++ src/routes/app/products_.$productId/route.ts
@@ -5,6 +5,7 @@ import { enterprises } from '../../../utils/constants/queryKeys/enterprise';
 import { QueryKey } from '@tanstack/react-query';
 import Page from '../../../utils/types/Page';
 import ProductResponseDto from '../../../utils/types/ProductResponseDto';
+import * as Sentry from '@sentry/react';
 
 const searchSchema = z.object({
   productModal: z.enum(['update', 'delete']).optional().catch(undefined),
@@ -18,12 +19,16 @@ export const Route = createFileRoute('/app/products/$productId')({
     const productPromise = queryClient.ensureQueryData({
       ...queries.product.detail(productId),
       initialData: () => {
-        for (const [key, value] of queryClient.getQueriesData<Page<ProductResponseDto>>({ queryKey: queries.product.page._def })) {
-          const item = value?.content.find((item) => item.id === productId);
-          if (item) {
-            initialDataKey = key;
-            return item;
+        try {
+          for (const [key, value] of queryClient.getQueriesData<Page<ProductResponseDto>>({ queryKey: queries.product.page._def })) {
+            const item = value?.content.find((item) => item.id === productId);
+            if (item) {
+              initialDataKey = key;
+              return item;
+            }
           }
+        } catch (error) {
+          Sentry.captureException(error, { data: { productPages: queryClient.getQueriesData({ queryKey: queries.product.page._def }) } });
         }
       },
       initialDataUpdatedAt: () => (initialDataKey ? queryClient.getQueryState(initialDataKey)?.dataUpdatedAt : undefined),
