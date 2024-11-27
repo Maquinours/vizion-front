5150e7ac6bbf7333ce928254506b420261198eb9
Maquinours <juju9559@hotmail.fr>
2024-11-27 14:02:48 +0100

    search FAQ fuzzy & non-fuzzy option

--- src/routes/app/faq/route.ts
+++ src/routes/app/faq/route.ts
@@ -6,13 +6,14 @@ const searchSchema = z.object({
   search: z.string().optional().catch(undefined),
   page: z.number().catch(0),
   archived: z.boolean().catch(false),
+  fuzzy: z.boolean().catch(false),
 });
 
 export const Route = createFileRoute('/app/faq')({
   validateSearch: (data: { search?: string; page?: number; archived?: boolean } & SearchSchemaInput) => searchSchema.parse(data),
-  loaderDeps: ({ search: { search, page, archived } }) => ({ search, page, size: 15, archived }),
-  loader: ({ context: { queryClient }, deps: { search, page, size, archived } }) => {
-    queryClient.ensureQueryData(faqs.page({ page, size })._ctx.byArchiveStateAndSearch(archived, search));
+  loaderDeps: ({ search: { search, page, archived, fuzzy } }) => ({ search, page, size: 15, archived, fuzzy }),
+  loader: ({ context: { queryClient }, deps: { search, page, size, archived, fuzzy } }) => {
+    queryClient.ensureQueryData(faqs.page({ page, size })._ctx.byArchiveStateAndSearch(archived, search, fuzzy));
   },
   staticData: {
     title: 'FAQ',
