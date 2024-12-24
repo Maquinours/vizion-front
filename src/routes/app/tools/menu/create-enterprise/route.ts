3c53b3354db9cbc249448b8033330dca6e9c3d11
Maquinours <juju9559@hotmail.fr>
2024-12-24 12:08:07 +0100

    create enterprise routes access fix

--- src/routes/app/tools/menu/create-enterprise/route.ts
+++ src/routes/app/tools/menu/create-enterprise/route.ts
@@ -1,15 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { queries } from '../../../../../utils/constants/queryKeys';
 import LoaderModal from '../../../../../components/LoaderModal/LoaderModal';
+import { queries } from '../../../../../utils/constants/queryKeys';
 import CategoryClient from '../../../../../utils/enums/CategoryClient';
-import { users } from '../../../../../utils/constants/queryKeys/user';
-import { redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/app/tools/menu/create-enterprise')({
-  beforeLoad: async ({ context: { queryClient } }) => {
-    const user = await queryClient.ensureQueryData(users.authentified());
-    if (!user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO')) throw redirect({ from: Route.fullPath, to: '..', search: true, replace: true });
-  },
   loader: ({ context: { queryClient } }) => {
     queryClient.prefetchQuery(queries.departments.list);
     queryClient.prefetchQuery(queries.enterprise.list._ctx.byCategory(CategoryClient.REPRESENTANT));
