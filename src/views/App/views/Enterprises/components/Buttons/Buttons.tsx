f6b797c5dd56dc62fcbfa4d81e64a9101524ad55
Maquinours <juju9559@hotmail.fr>
2024-12-24 16:55:37 +0100

    create enterprise button hide if non vizeo

--- src/views/App/views/Enterprises/components/Buttons/Buttons.tsx
+++ src/views/App/views/Enterprises/components/Buttons/Buttons.tsx
@@ -1,13 +1,18 @@
 import { Link } from '@tanstack/react-router';
+import { useAuthentifiedUserQuery } from '../../../../utils/functions/getAuthentifiedUser';
 
 const routePath = '/app/enterprises';
 
 export default function AppViewEnterprisesViewButtonsComponent() {
+  const { data: user } = useAuthentifiedUserQuery();
+
   return (
     <div className="flex justify-end gap-x-4">
-      <Link from={routePath} to="create" search replace resetScroll={false} preload="intent" className="btn btn-secondary">
-        Ajouter une entreprise
-      </Link>
+      {user.userInfo.roles.includes('ROLE_MEMBRE_VIZEO') && (
+        <Link from={routePath} to="create" search replace resetScroll={false} preload="intent" className="btn btn-secondary">
+          Ajouter une entreprise
+        </Link>
+      )}
     </div>
   );
 }
