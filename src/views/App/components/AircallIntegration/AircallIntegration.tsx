f15a918e47b63f374c9229bbdc553e60c7971c7c
Maquinours <juju9559@hotmail.fr>
2024-12-30 09:45:46 +0100

    hide aircall on small devices

--- src/views/App/components/AircallIntegration/AircallIntegration.tsx
+++ src/views/App/components/AircallIntegration/AircallIntegration.tsx
@@ -1,9 +1,12 @@
 import AircallPhone from 'aircall-everywhere';
 import classNames from 'classnames';
 import { useEffect, useState } from 'react';
+import { useWindowSize } from 'usehooks-ts';
 
 export default function AppViewAircallIntegrationComponent() {
   const [show, setShow] = useState(false);
+
+  const { width: windowWidth, height: windowHeight } = useWindowSize();
 
   useEffect(() => {
     const phone = new AircallPhone({
@@ -27,12 +30,12 @@ export default function AppViewAircallIntegrationComponent() {
   };
 
   return (
-    <div className="fixed bottom-0 right-0 z-50 m-4 flex flex-col items-end">
+    <div className={classNames('fixed bottom-0 right-0 z-50 m-4 flex flex-col items-end', { hidden: windowWidth < 400 || windowHeight < 800 })}>
       <div id="phone" className={classNames('rounded border border-solid border-[var(--grey-line)] bg-white shadow', { hidden: !show })}></div>
       <div className={classNames('w-8', { hidden: !show })}>
         <div className="m-auto h-0 w-0 border-x-8 border-t-8 border-solid border-x-transparent border-t-[var(--primary-color)]" />
       </div>
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={onClick} className={classNames({ hidden: windowWidth < 400 || windowHeight < 800 })}>
         <img src="https://aircall.github.io/aircall-everywhere/images/aircall-icon.svg" alt="Logo Aircall" width={32} height={32} />
       </button>
     </div>
