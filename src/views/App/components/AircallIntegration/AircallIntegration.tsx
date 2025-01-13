41b4e686a9fabfca94d259907d2fd8785b0ebf26
Maquinours <juju9559@hotmail.fr>
2025-01-13 08:55:38 +0100

    add console log test

--- src/views/App/components/AircallIntegration/AircallIntegration.tsx
+++ src/views/App/components/AircallIntegration/AircallIntegration.tsx
@@ -16,6 +16,7 @@ export default function AppViewAircallIntegrationComponent() {
         // ...
       },
       onLogout: () => {
+        console.log('onLogout');
         // ...
       },
     });
