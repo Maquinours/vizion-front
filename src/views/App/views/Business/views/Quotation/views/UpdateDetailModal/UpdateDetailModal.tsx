3322821d743af7c4953caad3e1fcbd78e2fdaac5
Maquinours <juju9559@hotmail.fr>
2025-03-14 12:39:20 +0100

    update quotation detail with vat

--- src/views/App/views/Business/views/Quotation/views/UpdateDetailModal/UpdateDetailModal.tsx
+++ src/views/App/views/Business/views/Quotation/views/UpdateDetailModal/UpdateDetailModal.tsx
@@ -88,6 +88,7 @@ export default function AppViewBusinessViewQuotationViewUpdateDetailModalView() 
         totalAmount,
         shippingServicePrice,
         virtualQty: detail.virtualQty,
+        vat,
       });
     },
     onSuccess: () => {
