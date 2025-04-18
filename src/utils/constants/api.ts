ed7a51f8e9ae3fa5e2d92fcb1e6cea7a37c8366f
Maquinours <juju9559@hotmail.fr>
2025-04-18 11:26:23 +0200

    API url fix

--- src/utils/constants/api.ts
+++ src/utils/constants/api.ts
@@ -1,7 +1,7 @@
 import { toBase64 } from 'js-base64';
 
 export const PUBLIC_BASE_URL = 'https://open-services-vizion.vizeo.eu/';
-export const PRIVATE_BASE_URL = 'https://test-services-vizion.vizeo.eu';
+export const PRIVATE_BASE_URL = 'https://services-vizion.vizeo.eu';
 export const AUTH_BASE_URL = 'https://auth-vizion.vizeo.eu/auth/realms/vizion-realm/protocol/openid-connect';
 export const FILE_STORAGE_BASE_URL = 'https://storage-vizion.vizeo.eu/api/uploads';
 export const FILE_READ_STORAGE_BASE_URL = 'https://storage-vizion.vizeo.eu/uploads';
@@ -16,4 +16,3 @@ export const AIRCALL_API_URL = 'https://api.aircall.io/v1';
 const AIRCALL_API_ID = '72c97b58603af07b9e590c8732ca112d';
 const AIRCALL_API_TOKEN = 'c3fd0fcb44337bba75a15c7e44e80188';
 export const AIRCALL_AUTHORIZATION = toBase64(`${AIRCALL_API_ID}:${AIRCALL_API_TOKEN}`);
- 
