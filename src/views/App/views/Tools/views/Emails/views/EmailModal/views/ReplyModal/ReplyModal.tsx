754a3186ebefa5defc5568a068897e6de79d0e28
Maquinours <juju9559@hotmail.fr>
2025-07-01 09:48:18 +0200

    Refactor email reply modal to use route loader
    Moved email data fetching from component to route loader using TanStack Router's loader API. This simplifies the ReplyModal component and ensures data is loaded before rendering.

--- src/views/App/views/Tools/views/Emails/views/EmailModal/views/ReplyModal/ReplyModal.tsx
+++ src/views/App/views/Tools/views/Emails/views/EmailModal/views/ReplyModal/ReplyModal.tsx
@@ -1,16 +1,12 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { getRouteApi } from '@tanstack/react-router';
 import ReplyEmailModalComponent from '../../../../../../../../../../components/ReplyEmailModal/ReplyEmailModal';
-import { queries } from '../../../../../../../../../../utils/constants/queryKeys';
 
 const routeApi = getRouteApi('/app/tools/emails/$emailId/reply');
 
 export default function AppViewToolsViewEmailsViewEmailModalViewReplyModalView() {
   const navigate = routeApi.useNavigate();
 
-  const { emailId } = routeApi.useParams();
-
-  const { data: email } = useSuspenseQuery(queries.emails.detail(emailId));
+  const { email } = routeApi.useLoaderData();
 
   return <ReplyEmailModalComponent isOpen onClose={() => navigate({ to: '..', search: true, replace: true, resetScroll: false })} email={email} />;
 }
