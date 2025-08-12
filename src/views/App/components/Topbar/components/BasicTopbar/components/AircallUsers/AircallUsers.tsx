1f557f6dcdfbdb134666cc672a6e8d1316a9392b
Maquinours <juju9559@hotmail.fr>
2025-08-12 16:31:39 +0200

    Hide AircallUsers if not aircall profile fix

--- src/views/App/components/Topbar/components/BasicTopbar/components/AircallUsers/AircallUsers.tsx
+++ src/views/App/components/Topbar/components/BasicTopbar/components/AircallUsers/AircallUsers.tsx
@@ -1,40 +1,42 @@
 import { useQuery } from '@tanstack/react-query';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tooltip } from 'react-tooltip';
 import { aircallQueryKeys } from '../../../../../../../../utils/constants/queryKeys/aircall';
+import { useAuthentifiedUserQuery } from '../../../../../../utils/functions/getAuthentifiedUser';
 
 export default function AppLayoutTopbarComponentBasicTopbarComponentAircallUsersComponent() {
-  // const { data: currentUser } = useAuthentifiedUserQuery();
+  const { data: currentUser } = useAuthentifiedUserQuery();
 
-  // const [isQueryEnabled, setIsQueryEnabled] = useState(true);
+  const [isQueryEnabled, setIsQueryEnabled] = useState(true);
 
   const { data: users } = useQuery({
     ...aircallQueryKeys.allUsers,
     select: (data) => data.users,
     staleTime: Infinity,
-    // enabled: isQueryEnabled,
+    enabled: isQueryEnabled,
   });
 
-  // useEffect(() => {
-  //   const enabled =
-  //     !users ||
-  //     users.some(
-  //       (user) => user.name.trim().toLowerCase() === `${currentUser.userInfo.firstName.trim()} ${currentUser.userInfo.lastName.trim()}`.trim().toLowerCase(),
-  //     );
-  //   setIsQueryEnabled(enabled);
-  // }, [users, currentUser]);
+  useEffect(() => {
+    const enabled =
+      !users ||
+      users.some(
+        (user) => user.name.trim().toLowerCase() === `${currentUser.userInfo.firstName.trim()} ${currentUser.userInfo.lastName.trim()}`.trim().toLowerCase(),
+      );
+    setIsQueryEnabled(enabled);
+  }, [users, currentUser]);
 
-  // if (
-  //   !users ||
-  //   !users.some(
-  //     (user) => user.name.trim().toLowerCase() === `${currentUser.userInfo.firstName.trim()} ${currentUser.userInfo.lastName.trim()}`.trim().toLowerCase(),
-  //   )
-  // )
-  //   return null;
+  if (
+    !users ||
+    !users.some(
+      (user) => user.name.trim().toLowerCase() === `${currentUser.userInfo.firstName.trim()} ${currentUser.userInfo.lastName.trim()}`.trim().toLowerCase(),
+    )
+  )
+    return null;
+
   return (
     <div className="flex flex-row gap-x-1">
-      {users?.map((user) => (
+      {users.map((user) => (
         <React.Fragment key={user.id}>
           <div
             className={classNames('rounded-lg p-1 text-white', {
