42562c8775a736f9fc7b0492b95780e416f3b334
Maquinours <juju9559@hotmail.fr>
2025-08-14 16:58:14 +0200

    aircall users show different availability status

--- src/views/App/components/Topbar/components/BasicTopbar/components/AircallUsers/AircallUsers.tsx
+++ src/views/App/components/Topbar/components/BasicTopbar/components/AircallUsers/AircallUsers.tsx
@@ -5,6 +5,13 @@ import { Tooltip } from 'react-tooltip';
 import { aircallQueryKeys } from '../../../../../../../../utils/constants/queryKeys/aircall';
 import { useAuthentifiedUserQuery } from '../../../../../../utils/functions/getAuthentifiedUser';
 
+const AVAILABILITY_DATA = [
+  { name: 'Disponible', value: 'available', backgroundColor: 'bg-green-400' },
+  { name: 'Déconnecté', value: 'offline', backgroundColor: 'bg-gray-400' },
+  { name: 'En appel', value: 'in_call', backgroundColor: 'bg-red-400' },
+  { name: 'Ne pas déra nger', value: 'do_not_disturb', backgroundColor: 'bg-yellow-400' },
+];
+
 export default function AppLayoutTopbarComponentBasicTopbarComponentAircallUsersComponent() {
   const { data: currentUser } = useAuthentifiedUserQuery();
 
@@ -14,8 +21,16 @@ export default function AppLayoutTopbarComponentBasicTopbarComponentAircallUsers
     ...aircallQueryKeys.allUsers,
     select: (data) => data.users,
     staleTime: Infinity,
+  });
+
+  const { data: availabilities } = useQuery({
+    ...aircallQueryKeys.availabilities._ctx.list,
+    select: (data) => data.users,
+    staleTime: Infinity,
     enabled: isQueryEnabled,
   });
+
+  const usersData = users?.map((user) => ({ ...user, availability_status: availabilities?.find((availability) => availability.id === user.id)?.availability }));
 
   useEffect(() => {
     const enabled =
@@ -36,24 +51,24 @@ export default function AppLayoutTopbarComponentBasicTopbarComponentAircallUsers
 
   return (
     <div className="flex flex-row gap-x-1">
-      {users.map((user) => (
-        <React.Fragment key={user.id}>
-          <div
-            className={classNames('rounded-lg p-1 text-white', {
-              'bg-green-400': user.available,
-              'bg-red-400': !user.available,
-            })}
-            data-tooltip-id={`aircall-user-${user.id}`}
-            data-tooltip-content={`${user.name} - ${user.available ? 'Disponible' : 'Indisponible'}`}
-          >
-            {user.name
-              .split(' ')
-              .map((i) => i[0].toUpperCase())
-              .join('')}
-          </div>
-          <Tooltip id={`aircall-user-${user.id}`} />
-        </React.Fragment>
-      ))}
+      {usersData?.map((user) => {
+        const availabilityData = AVAILABILITY_DATA.find((data) => data.value === user.availability_status);
+        return (
+          <React.Fragment key={user.id}>
+            <div
+              className={classNames('rounded-lg p-1 text-white', availabilityData?.backgroundColor)}
+              data-tooltip-id={`aircall-user-${user.id}`}
+              data-tooltip-content={`${user.name} - ${availabilityData?.name}`}
+            >
+              {user.name
+                .split(' ')
+                .map((i) => i[0].toUpperCase())
+                .join('')}
+            </div>
+            <Tooltip id={`aircall-user-${user.id}`} />
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
