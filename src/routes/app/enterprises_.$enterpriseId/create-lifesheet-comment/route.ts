2f493713f26f24c20db0b4809bbe2ae8bb2e6de1
Maquinours <79869121+Maquinours@users.noreply.github.com>
2025-10-27 11:14:24 +0100

    lifesheet concerned preload

--- src/routes/app/enterprises_.$enterpriseId/create-lifesheet-comment/route.ts
+++ src/routes/app/enterprises_.$enterpriseId/create-lifesheet-comment/route.ts
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
 import LoaderModal from '../../../../components/LoaderModal/LoaderModal';
+import { queries } from '../../../../utils/constants/queryKeys';
 
 export const Route = createFileRoute('/app/enterprises_/$enterpriseId/create-lifesheet-comment')({
+  loader: ({ context: { queryClient }, params: { enterpriseId } }) => {
+    queryClient.prefetchQuery(queries.profiles.list._ctx.byEnterpriseId(enterpriseId));
+  },
   pendingComponent: LoaderModal,
 });
